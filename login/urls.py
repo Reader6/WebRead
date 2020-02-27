04c3618482ce486e7a6bdbb10ce42df167f9ae18
login <724395215@qq.com>
2020-02-27 12:14:06 +0800

    制作网站主页面+添加登录注册功能

---
+++ login/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from login import views
+from django.contrib import admin
+app_name='login';
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('logout/', views.logout),
+    path('logins_search/',views.logins_search),
+]
