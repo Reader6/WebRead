f2bc26a04425b09e6815b2c9396bd10058995bc2
login <724395215@qq.com>
2020-06-25 21:34:16 +0800

    添加头像上传功能的相关视图

--- login/models.py
+++ login/models.py
@@ -14,12 +14,17 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     sex = models.CharField(max_length=32, choices=gender, default='男')
     c_time = models.DateTimeField(auto_now_add=True)
+    head_img=models.ImageField(upload_to="img",verbose_name="头像",null=True,blank=True)
 
+    def toDict(self):
+        return {'id':self.id,'username':self.name,'password':self.password,'email':self.email,'c_time':self.c_time,'head_img':self.head_img}
     def __str__(self):
         return self.name
 
     class Meta:
+        db_table='user'
         ordering = ['c_time']
         verbose_name = '用户'
         verbose_name_plural = '用户'
 # Create your models here.
+
